419b63896df1512bc56620e12338e01dbcf06b03
Andrew Smith <andrewts129@gmail.com>
2022-09-01 18:02:10 -0500

    Rework retroactive mixin to maintain some internal state about what time it 'is'

--- lib/retroactivity/retroactive.rb
+++ lib/retroactivity/retroactive.rb
@@ -3,20 +3,31 @@ module Retroactive
 
   included do
     after_save :_log_save
+    after_initialize { @frozen_at = nil }
 
     has_many :logged_changes, :as => :loggable, :class_name => "Retroactivity::LoggedChange"
 
-    def rewind!(as_of)
-      logged_changes.after(as_of).reverse_chronological.each(&:unapply!)
+    def as_of!(time)
+      if time < _current_time
+        logged_changes.after(time).reverse_chronological.each(&:unapply!)
+      else
+        raise NotImplementedError
+      end
+
+      @frozen_at = time
     end
 
     private
+
+    def _current_time
+      @frozen_at || Time.now
+    end
 
     def _log_save
       logged_changes.create!(
         :operation => Retroactivity::LoggedChange::Operation::UPDATE,
         :data => saved_changes,
-        :as_of => Time.now
+        :as_of => _current_time
       )
     end
   end
--- spec/retroactivity/retroactive_spec.rb
+++ spec/retroactivity/retroactive_spec.rb
@@ -16,38 +16,47 @@ RSpec.describe Retroactive do
 
   after(:all) { ActiveRecord::Base.connection.drop_table :test_klasses }
 
-  let(:instance) { Timecop.freeze(Date.new(2022, 1, 4)) { test_klass.create!(:foo => "bar") } }
+  let(:instance) { Timecop.freeze(creation_time) { test_klass.create!(:foo => "bar") } }
+  let(:creation_time) { Date.new(2022, 1, 4) }
 
   it "logs on saves" do
     expect { instance.update!(:foo => "baz") }.to change { instance.logged_changes.count }.from(1).to(2)
     expect(instance.foo).to eq("baz")
   end
 
-  describe "#rewind!" do
-    subject(:rewind!) { instance.rewind!(as_of) }
+  describe "#as_of!" do
+    subject(:as_of!) { instance.as_of!(as_of_time) }
 
     before do
-      Timecop.freeze(Date.new(2022, 1, 5)) { instance.update!(:foo => "bar2") }
-      Timecop.freeze(Date.new(2022, 1, 7)) { instance.update!(:foo => "bar3") }
-      Timecop.freeze(Date.new(2022, 1, 8)) { instance.update!(:foo => "bar4", :bar => 8) }
+      Timecop.freeze(creation_time + 1.day) { instance.update!(:foo => "bar2") }
+      Timecop.freeze(creation_time + 3.days) { instance.update!(:foo => "bar3") }
+      Timecop.freeze(creation_time + 4.days) { instance.update!(:foo => "bar4", :bar => 8) }
     end
 
-    let(:as_of) { Date.new(2022, 1, 6) }
+    let(:as_of_time) { creation_time + 2.days }
 
-    it "sets the attributes back to what they were at as_of" do
-      expect { rewind! }
+    it "sets the attributes back to what they were at that time" do
+      expect { as_of! }
         .to change { instance.foo }.from("bar4").to("bar2")
         .and change { instance.bar }.from(8).to(nil)
     end
 
     context "when going back to before the object was created" do
-      let(:as_of) { Date.new(2022, 1, 1) }
+      let(:as_of_time) { creation_time - 1.day }
 
       it "sets everything to nil" do
-        expect { rewind! }
+        expect { as_of! }
           .to change { instance.id }.to(nil)
           .and change { instance.foo }.from("bar4").to(nil)
           .and change { instance.bar }.from(8).to(nil)
+      end
+    end
+
+    context "when travelling into the future" do
+      let(:as_of_time) { Time.now + 5.days }
+
+      it "raises an error" do
+        expect { as_of! }.to raise_error(NotImplementedError)
       end
     end
   end
